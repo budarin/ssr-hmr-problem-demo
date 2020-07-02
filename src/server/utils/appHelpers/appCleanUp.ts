658c1410fb186d909984b1123af0db76fc034150
Vadim Budarin <budarin-vv@yandex.ru>
2020-07-02 14:53:03 +0300

    fix: remove redis

--- src/server/utils/appHelpers/appCleanUp.ts
+++ src/server/utils/appHelpers/appCleanUp.ts
@@ -1,10 +1,7 @@
-import redis from '../../services/redis';
 import logger from '../../services/logger';
 
 const appCleanUp = (): void => {
     logger.trace('appCleanUp');
-
-    redis.disconnect();
 };
 
 export default appCleanUp;
